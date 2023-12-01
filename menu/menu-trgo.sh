2749cce69de779e29332f254b4749c9659773526
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-01 22:07:11 +0700

    Update menu-trgo.sh

--- menu/menu-trgo.sh
+++ menu/menu-trgo.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 MYIP=$(wget -qO- ipinfo.io/ip);
-
 # // color
 GREENBO="\e[92;1m"
 NC='\e[0m' # penutup
@@ -8,23 +7,20 @@ BGGREEN="\033[42;37m" # hijau
 BGRED="\033[41;37m" # merah
 BGYELLOW="\033[43;37m" # kuning
 BGPURPLE="\033[45;37m" # ungu
-
-clear 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      • TROJAN Go MENU •          \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e ""
-echo -e " [\e[36m•1\e[0m] Create Account Trojan Go "
-echo -e " [\e[36m•2\e[0m] Trial Account Trojan Go "
-echo -e " [\e[36m•3\e[0m] Extending Account Trojan Go Active Life "
-echo -e " [\e[36m•4\e[0m] Delete Account Trojan Go "
-echo -e " [\e[36m•5\e[0m] Check User Login Trojan Go"
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+clear
+grenbo="\e[92;1m"
+NC='\033[0m'
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          MENU MANAGER TR                 $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}1.)${NC} \033[0;36mCreating Trojan-GO${NC}"
+echo -e "\033[1;93m│  ${grenbo}2.)${NC} \033[0;36mTrial Trojan-GO${NC}"
+echo -e "\033[1;93m│  ${grenbo}3.)${NC} \033[0;36mExtend Trojan-GO${NC}"
+echo -e "\033[1;93m│  ${grenbo}4.)${NC} \033[0;36mDelete Trojan-GO${NC}"
+echo -e "\033[1;93m│  ${grenbo}5.)${NC} \033[0;36mCheck login Trojan-GO${NC}"
+echo -e "\033[1;93m│  ${grenbo}0.)${NC} \033[0;36mComeback To Menu${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
 echo -e ""
 read -p " Select menu : " opt
 echo -e ""
