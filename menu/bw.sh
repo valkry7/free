18c2ad50f8749a21cf36cca5652c825841c54585
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-02 00:00:52 +0700

    Update bw.sh

--- menu/bw.sh
+++ menu/bw.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-
-clear 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m      • BANDWITH MONITOR •         \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "" 
-echo -e " [\e[36m•1\e[0m] Lihat Total Bandwith Tersisa"
-echo -e " [\e[36m•2\e[0m] Tabel Penggunaan Setiap 5 Menit"
-echo -e " [\e[36m•3\e[0m] Tabel Penggunaan Setiap Jam"
-echo -e " [\e[36m•4\e[0m] Tabel Penggunaan Setiap Hari"
-echo -e " [\e[36m•5\e[0m] Tabel Penggunaan Setiap Bulan"
-echo -e " [\e[36m•6\e[0m] Tabel Penggunaan Setiap Tahun"
-echo -e " [\e[36m•7\e[0m] Tabel Penggunaan Tertinggi"
-echo -e " [\e[36m•8\e[0m] Statistik Penggunaan Setiap Jam"
-echo -e " [\e[36m•9\e[0m] Lihat Penggunaan Aktif Saat Ini"
-echo -e " [\e[36m10\e[0m] Lihat Trafik Penggunaan Aktif Saat Ini [5s]"
-echo -e "" 
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e " [\e[31m•x\e[0m] Keluar"
-echo -e "" 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+#MYIP=$(wget -qO- ipinfo.io/ip);
+grenbo="\e[92;1m"
+NC='\e[0m' # penutup
+clear
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          MENU MANAGER BW                 $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}1.${NC} \033[0;36mTotal Bandwith${NC}"
+echo -e "\033[1;93m│  ${grenbo}2.${NC} \033[0;36mSwap 5 min${NC}"
+echo -e "\033[1;93m│  ${grenbo}3.${NC} \033[0;36mSwap 1 hours${NC}"
+echo -e "\033[1;93m│  ${grenbo}4.${NC} \033[0;36mSwap 1 Mount${NC}"
+echo -e "\033[1;93m│  ${grenbo}5.${NC} \033[0;36mSwap 1 Years${NC}"
+echo -e "\033[1;93m│  ${grenbo}0.${NC} \033[0;36mComeback${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
 echo -e ""
 read -p " Select menu : " opt
 echo -e ""
