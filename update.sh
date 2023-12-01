84b3eb774eab8cc07ce49401b58d2f77c320a6f6
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-01 19:56:58 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /usr/bin
+rm -rf menu
+rm -rf menu-ssh
+rm -rf menu-trojan
+rm -rf menu-vmess
+rm -rf menu-vless
+rm -rf menu-trgo
+rm -rf menu-backup
+
+cd /usr/bin
+echo -e "==============[ UPDATE MENU ]=============="
