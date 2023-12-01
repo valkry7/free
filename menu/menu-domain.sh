fcf0a7c2ab3cc87ed26859ff367288f2979c216d
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-01 23:55:48 +0700

    Update menu-domain.sh

--- menu/menu-domain.sh
+++ menu/menu-domain.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 # // izin
-MYIP=$(wget -qO- ipinfo.io/ip);
-# // color
-GREENBO="\e[92;1m"
+grenbo="\e[92;1m"
 NC='\e[0m' # penutup
-BGGREEN="\033[42;37m" # hijau
-BGRED="\033[41;37m" # merah
-BGYELLOW="\033[43;37m" # kuning
-BGPURPLE="\033[45;37m" # ungu
-
-clear 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;100;33m        • DOMAIN MENU •            \E[0m"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "" 
-echo -e " [\e[36m•1\e[0m] CHANGE DOMAIN VPS"
-echo -e " [\e[36m•2\e[0m] Renew Certificate DOMAIN"
-echo -e ""
-echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
-echo -e   ""
-echo -e   "Press x or [ Ctrl+C ] • To-Exit"
-echo -e   ""
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+clear
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m          MENU MANAGER HOST               $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}1.${NC} \033[0;36mAdd Domain Host${NC}"
+echo -e "\033[1;93m│  ${grenbo}2.${NC} \033[0;36mRenew Cert Domain${NC}"
+echo -e "\033[1;93m│  ${grenbo}0.${NC} \033[0;36mComeback${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
 echo -e ""
 read -p " Select menu : " opt
 echo -e ""
