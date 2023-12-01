dd4508ce08e297d0513cb4cede235dde83b58588
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-01 21:05:18 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,41 +11,42 @@ sleep 2
 G="\033[1;32m"
 N="\e[0m"
 echo -e "${G}===================[ UPDATE MENU ]=====================${N}"
+sleep 3
 cd /usr/bin
 # menu
 wget -O menu "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu.sh"
 wget -O menu-backup "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-backup.sh"
 wget -O menu-vmess "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-vmess.sh"
 wget -O menu-vless "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-vless.sh"
-wget -O running "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/clearcache.sh"
+#wget -O running "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/running.sh"
+#wget -O clearcache "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/clearcache.sh"
 wget -O menu-trgo "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-trgo.sh"
 wget -O menu-trojan "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-trojan.sh"
 
 # menu ssh ovpn
 wget -O menu-ssh "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-ssh.sh"
 wget -O usernew "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/tendang.sh"
+#wget -O trial "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/trial.sh"
+#wget -O renew "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/renew.sh"
+#wget -O hapus "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/hapus.sh"
+#wget -O cek "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/cek.sh"
+#wget -O member "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/member.sh"
+#wget -O delete "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/delete.sh"
+#wget -O autokill "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/autokill.sh"
+#wget -O ceklim "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/ceklim.sh"
+#wget -O tendang "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/tendang.sh"
 
 # menu system
 wget -O menu-set "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-set.sh"
 wget -O menu-domain "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/valkry7/free/SUDEV/port/port-change.sh"
+#wget -O add-host "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/add-host.sh"
+#wget -O port-change "https://raw.githubusercontent.com/valkry7/free/SUDEV/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/valkry7/free/SUDEV/xray/certv2ray.sh"
 wget -O menu-webmin "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/menu-webmin.sh"
-wget -O speedtest "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/restart.sh"
+#wget -O speedtest "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/speedtest_cli.py"
+#wget -O about "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/about.sh"
+#wget -O auto-reboot "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/auto-reboot.sh"
+#wget -O restart "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/restart.sh"
 wget -O bw "https://raw.githubusercontent.com/valkry7/free/SUDEV/menu/bw.sh"
 # // Backup
 wget -O autobackup "https://raw.githubusercontent.com/valkry7/free/SUDEV/backup/autobackup.sh"
@@ -55,11 +56,11 @@ wget -O strt "https://raw.githubusercontent.com/valkry7/free/SUDEV/backup/strt.s
 wget -O limitspeed "https://raw.githubusercontent.com/valkry7/free/SUDEV/backup/limitspeed.sh"
 
 # change port
-wget -O port-ssl "https://raw.githubusercontent.com/valkry7/free/SUDEV/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/valkry7/free/SUDEV/port/port-ovpn.sh"
+#wget -O port-ssl "https://raw.githubusercontent.com/valkry7/free/SUDEV/port/port-ssl.sh"
+#wget -O port-ovpn "https://raw.githubusercontent.com/valkry7/free/SUDEV/port/port-ovpn.sh"
 wget -O xp "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/valkry7/free/SUDEV/acs-set.sh"
-wget -O sshws "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/sshws.sh"
+#wget -O acs-set "https://raw.githubusercontent.com/valkry7/free/SUDEV/acs-set.sh"
+#wget -O sshws "https://raw.githubusercontent.com/valkry7/free/SUDEV/ssh/sshws.sh"
 
 chmod +x menu
 chmod +x menu-backup
