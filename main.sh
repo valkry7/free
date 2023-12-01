217885b9adc907c437025e24d042f371095d15db
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-01 16:22:03 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -71,9 +71,9 @@ if [ "" = "$PKG_OK" ]; then
   echo -e "[ ${tyblue}NOTES${NC} ] Then run this script again"
   echo -e "[ ${tyblue}NOTES${NC} ] TAU ENTER KAN ?? YA ENTER LAH BABI"
   read
-fi
+# else
   echo -e "[ ${green}INFO${NC} ] Oke installed"
-
+ fi
 ttet=`uname -r`
 ReqPKG="linux-headers-$ttet"
 if ! dpkg -s $ReqPKG  >/dev/null 2>&1; then
