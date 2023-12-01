186fc576b719eaa745cad6ff5702f14f72ae43b1
valkry7 <151914434+valkry7@users.noreply.github.com>
2023-12-01 16:10:39 +0700

    Update LT.sh

--- LT.sh
+++ LT.sh
@@ -69,7 +69,7 @@ if [ "" = "$PKG_OK" ]; then
   echo -e "[ ${tyblue}NOTES${NC} ] After rebooting"
   sleep 1
   echo -e "[ ${tyblue}NOTES${NC} ] Then run this script again"
-  echo -e "[ ${tyblue}NOTES${NC} ] if you understand then tap enter now"
+  echo -e "[ ${tyblue}NOTES${NC} ] TAU ENTER KAN ?? YA ENTER LAH BABI"
   read
 else
   echo -e "[ ${green}INFO${NC} ] Oke installed"
